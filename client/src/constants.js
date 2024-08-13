3ab70a1255e4e431612c956d8c42ca4b89df5c39
Akshat Kumar <akshatkumar10203@gmail.com>
2024-08-13 10:22:42 +0530

    changes

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,4 +1,4 @@
 // constants.js
-const BASE_URL = 'http://localhost:3000';
-//https://edumate-tutor-api.vercel.app   
+const BASE_URL = 'http://motor-q.vercel.app';
+//https://edumate-tutor-api.vercel.app    http://localhost:3000
 export default BASE_URL;
--- server/index.js
+++ server/index.js
@@ -10,12 +10,12 @@ import AppointmentModel from "./models/Appointment.model.js";
 import carRoutes from "./routes/carRoutes.js";
 import driverRoutes from "./routes/driverRoutes.js";
 dotenv.config();
-//   https://edumate-tutor.vercel.app
+//   https://edumate-tutor.vercel.app http://localhost:5173
 const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://motor-q-app.vercel.app",
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true,
   })
